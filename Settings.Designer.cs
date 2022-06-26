4295cdee902450ce6eeca123ad4240aa72995162
kawalerooo <73931905+kawalerooo@users.noreply.github.com>
2022-06-26 21:04:28 +0200

    Create Settings.Designer.cs

---
+++ Settings.Designer.cs
@@ -0,0 +1,27 @@
+namespace PSW_lab4.Properties {
+    
+    
+    [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
+        
+        private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
+        
+        public static Settings Default {
+            get {
+                return defaultInstance;
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("user id=root;server=127.0.0.1;database=pswlab4;persistsecurityinfo=True;passwor" +
+            "d=1234")]
+        public string psw_lab4ConnectionString {
+            get {
+                return ((string)(this["psw_lab4ConnectionString"]));
+            }
+        }
+    }
+}
