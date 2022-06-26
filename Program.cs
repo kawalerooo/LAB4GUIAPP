137a28213d2150ec9d5a7a7d881eae8e9ef0d83b
kawalerooo <73931905+kawalerooo@users.noreply.github.com>
2022-06-26 21:01:03 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Windows.Forms;
+
+namespace PSW_lab4
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        /// 
+
+
+
+
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            
+
+
+            MailSender mailSender = new MailSender();
+        }
+        
+    }
+}
